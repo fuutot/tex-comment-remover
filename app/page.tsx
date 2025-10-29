90644cfce3ab85d0437424058ac0621dd746f5e0
fuutot <fuutt659@gmail.com>
2025-10-29 17:06:44 +0900

    update: 送信ボタンとしての動作を防止

--- app/page.tsx
+++ app/page.tsx
@@ -47,6 +47,7 @@ export default function Home() {
 
       {/* ボタン */}
       <button
+        type="button" // 送信ボタンとしての動作を防止
         onClick={handleRemoveComments}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
       >
